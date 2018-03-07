d91aa5964eaeab3e717fa12db12d175b2ebab022
Austin Wagner <austin.e.wagner@gmail.com>
2018-03-06 21:33:45 -0800

    make it better

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,7 @@ export const DELETE_POST = 'delete_post';
 
 
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=picklerick';
 
 export function fetchPosts() {
